69db730c79fd2dd3b57257e709cc79691c54cd8b
Ibrahim Halil Siyli <ihalilsiyli@gmail.com>
2025-03-11 22:37:36 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,6 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:  240543022
+# Your Name and Surname:  IBRAHIM HALIL SIYLI
+import sys
+
+print("Python version:", f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
+print("Version info:", sys.version_info)
