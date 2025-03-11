bbaa383301928827e4ac345d6db5304607cbba43
Ibrahim Halil Siyli <ihalilsiyli@gmail.com>
2025-03-11 23:54:34 +0300

    Update ex006.py

--- Exercises/ex006.py
+++ Exercises/ex006.py
@@ -1,2 +1,21 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:  240543022
+# Your Name and Surname:  IBRAHIM HALIL SIYLI
+original_list = list(range(1, 11))
+
+print(original_list)                                #1.Print the original list
+
+reverse_list = original_list[::-1]
+print(reverse_list)                                 #2.Reverse the list and print it
+
+added_reverse_list = reverse_list + [11,12,13]
+
+print(added_reverse_list )                          #3.Add the numbers 11, 12, and 13 to the list and print it(reversed)
+print(len(added_reverse_list ))                     #4.Print the list's length
+
+removed_list = added_reverse_list[1:-1]
+print(removed_list)                                 #5.Remove the first and last elements, then print the list
+
+even_list= original_list[1::2]
+print(even_list)                                    #6.Create and print a new sorted list containing only even numbers from the original list
+
+
