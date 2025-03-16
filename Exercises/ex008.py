9c0b0c6904cfa45831667ec36d7988cdec0992f4
Ibrahim Halil Siyli <ihalilsiyli@gmail.com>
2025-03-16 20:03:02 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,5 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 240543022
+# Your Name and Surname:IBRAHIM HALIL SIYLI
 _number = int(input("Enter a number to specify the number of rows for the pyramid pattern:  "))
 
 for i in range(1, _number+1):
